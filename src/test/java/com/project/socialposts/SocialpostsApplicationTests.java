4a89f89fe86787c6f15bafca0c41e661d42a1d48
Guilber Correia <guilber.correia.gc@gmail.com>
2023-09-13 16:11:06 -0300

    Criação do projeto e configuração inicial

---
+++ src/test/java/com/project/socialposts/SocialpostsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.socialposts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialpostsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
