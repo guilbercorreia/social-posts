ab0f9968d28cfc452cd3ac0235a3d2b85e286ed6
Guilber Correia <guilber.correia.gc@gmail.com>
2023-09-14 13:42:19 -0300

    Atualizar seed

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,2 @@
-INSERT INTO tb_users (id, name, email) VALUES (1, 'João da Silva', 'joao.silva@example.com');
-INSERT INTO tb_users (id, name, email) VALUES (2, 'Maria Pereira', 'maria.pereira@example.com');
+INSERT INTO tb_users (name, email) VALUES ('João da Silva', 'joao.silva@example.com');
+INSERT INTO tb_users (name, email) VALUES ('Maria Pereira', 'maria.pereira@example.com');
